45846540d73c7eadcc89f53b0c554208ab200062
Artiishock <79396846+Artiishock@users.noreply.github.com>
2025-11-04 17:45:12 +0300

    123

---
+++ app/Http/Controllers/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        // Для формы Statamic
+        '!/Statamic/forms/submit/property_form',
+        
+        // Ваши существующие исключения (перенесены из другого файла)
+        'api/*',
+        'telegram-webhook',
+        '/api/telegram-property',
+        '/api/telegram-property/*',
+        'telegram-property*'
+    ];
+}
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,6 +1,25 @@
 <?php
-protected $middleware = [
-    \Fruitcake\Cors\HandleCors::class,
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middlewareGroups = [
+          \Fruitcake\Cors\HandleCors::class,
       \App\Http\Middleware\GlideFallback::class,
-    // другие middleware
-];
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class, // ⬅️ ЭТА СТРОКА ДОЛЖНА БЫТЬ
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            // ...
+        ],
+    ];
+}
